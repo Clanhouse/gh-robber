8c5599d554c00842323edf5329d399192cb027b7
Krzysztof Wolny <krzysztof.wolny@outlook.com>
2021-05-11 11:01:10 +0200

    1: theme and theme provider added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from 'react';
+import { theme } from './styles/Theme';
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
   return (
     <div className="App">
-     
+        <ThemeProvider theme={theme}>
+          smt
+        </ThemeProvider>
     </div>
   );
 }
---
+++ client/src/styles/Theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+	colors: {
+		white: "#FFF",
+		black: "#000",
+		orange: "#F06449",
+		issabeline: "#F0EFEF",
+		gainsbro: "#DADAD9",
+		darkGray: "#524E4E",
+		lightGray: "#524E4E",
+		blackOlive: "#36382E",
+		blue: "#5BC3EB",
+	},
+};

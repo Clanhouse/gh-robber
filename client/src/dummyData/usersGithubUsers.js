9b90e9a946ed2c191f1c3c8ddf452201a0891a6b
Hanna Kozak <hannawkozak@gmail.com>
2021-07-05 22:27:24 +0100

    update dummy data

--- client/src/dummyData/usersGithubUsers.js
+++ client/src/dummyData/usersGithubUsers.js
@@ -1,37 +1,37 @@
 export const usersGithubUsers = [
-    {
+    {   id: 1,
         username: 'user1',
         repositories_count: 324
     },
-    {
+    {   id: 2,
         username: 'user2',
         repositories_count: 535
     },
-    {
+    {   id: 3,
         username: 'user3',
         repositories_count: 54567
     },
-    {
+    {   id: 4,
         username: 'user5',
         repositories_count: 4235534
     },
-    {
+    {   id: 5,
         username: 'user6',
         repositories_count: 323
     },
-    {
+    {   id: 6,
         username: 'user7',
         repositories_count: 3
     },
-    {
+    {   id: 7,
         username: 'user8',
         repositories_count: 1
     },
-    {
+    {   id: 8,
         username: 'user9',
         repositories_count: 3
     },
-    {
+    {   id: 9,
         username: 'user10',
         repositories_count: 69
     },

a7bb689564fd139efd937d2ae983fe92d6f481bb
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-08-05 05:42:26 +0200

    Create a group of commands
    Creating a group of commands to operate the database

--- backend/app/db_manage_commands.py
+++ backend/app/db_manage_commands.py
@@ -1,15 +1,38 @@
 from pathlib import Path
 import json
 
-from . import db, app
-from .models import GithubUserInfo
+from backend.app import db, app
+from backend.app import GithubUserInfo
+
+
+@app.cli.group()
+def db_manage():
+    """Database management command"""
+    pass
 
 
 @db.manage.command()
 def add_data():
     """Add sample data to database"""
-    user_info = Path(__file__).parent / "data_sample"/ "user_info.json"
-    with open(user_info) as file:
-        data_json = json.load(file)
-    for item in data_json:
-        pass
+    try:
+        user_info = Path(__file__).parent / "data_sample" / "user_info.json"
+        with open(user_info) as file:
+            data_json = json.load(file)
+        for item in data_json:
+            user_info = GithubUserInfo(**item)
+            db.session.add(user_info)
+        db.session.commit()
+        print("Data has been successfully added to database")
+    except Exception as exc:
+        print(f"Unexpected error: {exc}")
+
+
+@db.manage.command()
+def remove_data():
+    """Remove all data form database"""
+    try:
+        db.session.execute("TRUNCATE TABLE user_info")
+        db.session.cmmit()
+        print("Data has been successfully removed from database")
+    except Exception as exc:
+        print(f"Unexpected error: {exc}")

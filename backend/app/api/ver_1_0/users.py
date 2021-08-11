74473b850e37c3648764cd21cac8119318dfae41
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-08-11 12:23:54 +0200

    Added sorting and dynamic json creation

--- backend/app/api/ver_1_0/users.py
+++ backend/app/api/ver_1_0/users.py
@@ -1,6 +1,6 @@
 from backend.app.api.ver_1_0 import users_api
-from backend.app.models import User, GithubUser
-
+from backend.app.models import User, GithubUser, GithubUserInfo, GithubUserInfoSchema, info_schema
+from flask import jsonify, request
 
 @users_api.route("/users/")
 def get_users():
@@ -12,3 +12,17 @@ def get_users():
 def get_github_users():
     github_users = [github_user.username for github_user in GithubUser.query.all()]
     return {"github_users": github_users}
+
+@users_api.route("/search/", methods=['GET'])
+def users_info():
+    query = GithubUserInfo.query
+    schema_args = GithubUserInfo.get_args(request.args.get("fields"))
+    query = GithubUserInfo.apply_order(query, request.args.get("sort"))
+    users = query.all()
+    user_schema = GithubUserInfoSchema(**schema_args)
+
+    return jsonify({
+        "success": True,
+        "data": user_schema.dump(users),
+        "numbers_of_records": len(users)
+    })

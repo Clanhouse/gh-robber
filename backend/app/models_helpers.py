a8b62b93d7e6bf5ef703002bdc7fd7a3d9923119
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-08-11 12:12:54 +0200

    Add create_fake_info method

--- backend/app/models_helpers.py
+++ backend/app/models_helpers.py
@@ -1,10 +1,12 @@
-from random import seed, randint
-
+from random import seed, randint, choice
 import forgery_py
 from sqlalchemy.exc import IntegrityError
 
 from . import db
-from .models import User, GithubUser
+from .models import User, GithubUser, GithubUserInfo, GithubUserInfoSchema
+
+
+LANGUAGE_LIST = ["JavaScript", "Java", "Python", "PHP", "C++", "C#", "TypeScript", "R", "Swift", "Kotlin", "Rust", "Julia"]
 
 
 def create_fake_data(count):
@@ -18,6 +20,24 @@ def create_fake_data(count):
             repositories_count=randint(1, 3),
         )
         db.session.add(fake_github_user)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+
+
+def create_fake_info(count=10):
+    """Create sample data to database"""
+    seed()
+    for _ in range(count):
+        fake_info = GithubUserInfo(
+            username = forgery_py.internet.username(True),
+            language = choice(LANGUAGE_LIST),
+            date = forgery_py.date.day(),
+            stars = randint(1, 100),
+            number_of_repositories = randint(1, 10),
+        )
+        db.session.add(fake_info)
     try:
         db.session.commit()
     except IntegrityError:

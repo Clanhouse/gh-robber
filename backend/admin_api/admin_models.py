91c0958f0a4c8d05d91e74e4c2044068e1532815
Jordan Brocker Rudow <70343977+Trianglium@users.noreply.github.com>
2021-12-09 10:44:24 -0600

    update admin_models - add template names to gh model

--- backend/admin_api/admin_models.py
+++ backend/admin_api/admin_models.py
@@ -46,6 +46,8 @@ class GithubUserInfo(db.Model):
 
 class UserAccountView(sqla.ModelView):
     list_template = 'templates/db_views/list_users.html'
+    create_template = 'templates/db_views/create_users.html'
+    edit_template = 'templates/db_views/edit_users.html'
     column_searchable_list = ('id','name', 'email')
     column_list = ('id','name', 'email', 'password')
     column_display_pk = True
@@ -53,6 +55,9 @@ class UserAccountView(sqla.ModelView):
     form_columns = ['name', 'email', 'password']
 
 class GHUsersInfoView(sqla.ModelView):
+    list_template = 'templates/db_views/list_users.html'
+    create_template = 'templates/db_views/create_users.html'
+    edit_template = 'templates/db_views/edit_users.html'
     column_searchable_list = ('id','username', 'repository', 'languages', 'date','number_of_repositories')
     column_list = ('id','username', 'repository', 'languages', 'date', 'number_of_repositories', 'stars')
     column_display_pk = True

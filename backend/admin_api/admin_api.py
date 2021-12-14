fcf05b57de0c9e4d5f48a4f31724426f7507a4d4
Jordan Brocker Rudow <70343977+Trianglium@users.noreply.github.com>
2021-12-14 13:08:40 -0600

    Update Admin_api
    relocate views to separate file & correct imports

--- backend/admin_api/admin_api.py
+++ backend/admin_api/admin_api.py
@@ -12,7 +12,7 @@ from flask_admin.menu import MenuCategory, MenuView, MenuLink, SubMenuCategory
 from flask.views import MethodView
 from flask import render_template
 
-
+# TO DO - delete all config settings nelow, except flask_admin_swatch
 app = Flask(__name__)
 app.config['FLASK_ADMIN_SWATCH'] = 'lux'
 app.config['SECRET_KEY'] = '123456790'
@@ -21,29 +21,18 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-# To do: Change this route when integrating other GH Robber components.
-# If you just want to test GHRobber Admin API for functionality and etc, keep this the same. Run module on it's own.
-
-@app.route('/')
-def index():
-    return '<a href="/admin/">Click me to get to Admin!</a>'
-
-
 admin = Admin(app, base_template='admin/custom_base.html', index_view=AdminIndexView(name='Dashboard', template='dashboard.html'), template_mode='bootstrap4')
 
-from .admin_api import admin_models
-from ..admin_models import UserAccountView, User, GHUsersInfoView, GithubUserInfo, AuthView, DocsView
-    
-admin.add_view(DocsView(name="Documentation", endpoint='/docs'))
+from admin_models import UserAccountView, GHUsersInfoView
+from app.models import User, GithubUserInfo
+
 admin.add_view(UserAccountView(User, db.session, name="User Management"))
 admin.add_view(GHUsersInfoView(GithubUserInfo, db.session, name="Github Management"))
-admin.add_view(GHUsersAPIView(name="API", endpoint="/api/v1/users"))
-admin.add_view(AuthView(name="Authentication", endpoint='/auth'))
 
 
 if __name__ == '__main__':
 
-    # the test database
+    # the test database - to be deleted
     db.create_all()
     
     app.run(debug=True)
